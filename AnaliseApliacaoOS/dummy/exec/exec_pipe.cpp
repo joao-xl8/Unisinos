998b6ffcb48a6403f7d527d076375e4e7e20c0c8
xl8 <joaopaulo167@gmail.com>
2026-03-04 01:21:05 -0300

    exec_pipe example

--- AnaliseApliacaoOS/dummy/exec/exec_pipe.cpp
+++ AnaliseApliacaoOS/dummy/exec/exec_pipe.cpp
@@ -6,3 +6,38 @@
 // The main idea is for the main program create the two processes to exec grp and ping 
 // and create the pipe for them.
 
+#include <iostream>
+#include <unistd.h>
+#include <time.h>
+#include <sys/wait.h>
+
+int main () {
+    int fd[2];
+    if (pipe(fd) == -1) return 1;
+
+    pid_t pid1 = fork();
+    if (pid1 < 0) return 2;
+
+    if (pid1 == 0) {
+        dup2(fd[1], STDOUT_FILENO); // overwrites the stdout with the file descriptor write
+        close(fd[0]);
+        close(fd[1]);
+        execlp("ping", "ping", "-c", "3", "google.com", NULL);
+    }
+
+    pid_t pid2 = fork();
+    if (pid2 < 0) return 3;
+
+    if (pid2 == 0) {
+        dup2(fd[0], STDIN_FILENO); // overwrites the stdin with the file descriptor read
+        close(fd[0]);
+        close(fd[1]);
+        execlp("grep", "grep", "rtt", NULL);
+    }
+
+    close(fd[0]);
+    close(fd[1]);
+    waitpid(pid1, nullptr, 0);
+    waitpid(pid2, nullptr, 0);
+    return 0;
+}
