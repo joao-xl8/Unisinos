bd5b51f6a3f7628c9f5b6e663a70158f71153cd2
xl8 <joaopaulo167@gmail.com>
2026-03-03 23:32:51 -0300

    logs for pipes

--- AnaliseApliacaoOS/dummy/fork/fork_two_way_pipe.cpp
+++ AnaliseApliacaoOS/dummy/fork/fork_two_way_pipe.cpp
@@ -27,9 +27,11 @@ int main(int argc, char *arcv[])
         int message_len;
         if (read(p2[0], &message_len, sizeof(message_len)) == -1)
             return 6;
+        printf("Read the message size from the pipe\n");
         std::string message(message_len, '\n');
         if (read(p2[0], &message[0], message_len) == -1)
             return 6;
+
         printf("Child received a message: %s\n", message.c_str());
         size_t pos = message.find("parent");
         if (pos != std::string::npos)
@@ -39,6 +41,7 @@ int main(int argc, char *arcv[])
         message_len = message.size();
         if (write(p1[1], &message_len, sizeof(message_len)) == -1)
             return 7;
+        printf("Wrote the message size from the children to the pipe\n");
         if (write(p1[1], message.c_str(), message_len) == -1)
             return 7;
         printf("Child will send a message: %s\n", message.c_str());
@@ -54,11 +57,13 @@ int main(int argc, char *arcv[])
         int message_len = message.size(); // A pipe is just a stream of raw bytes and does not preserve message boundaries, so the receiving process has no way to know where one message ends. If you only send the characters, read() doesn’t know how many bytes belong to that string. Sending the length first tells the receiver exactly how many bytes to read, ensuring the full message is reconstructed correctly.
         if (write(p2[1], &message_len, sizeof(message_len)) == -1)
             return 4;
+        printf("Wrote the message size to the pipe\n");
         if (write(p2[1], message.c_str(), message_len) == -1)
             return 4;
         // after writing the child will manipulate the string and we will read.
         if (read(p1[0], &message_len, sizeof(message_len)) == -1)
             return 5;
+        printf("Read the message size from children response\n");
         std::string new_message(message_len, '\n');
         if (read(p1[0], &new_message[0], message_len) == -1)
             return 5;
